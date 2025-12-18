baef712bad58550689523b2d27a3e90b285375b1
Nabin Raut <nabinraut221@gmail.com>
2025-12-18 13:32:02 +0545

    fixing key issues

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class WeatherApp(QWidget):
         self.get_weather_button.clicked.connect(self.get_weather)
 
     def get_weather(self):
-        api_key = "aa001bcb466041ac2424d37d17446c76"
+        api_key = "enter your api key ."
         city = self.city_input.text()
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
@@ -171,4 +171,5 @@ if __name__ == "__main__":
     weather_app = WeatherApp()
     weather_app.show()
     sys.exit(app.exec_())
+
 
